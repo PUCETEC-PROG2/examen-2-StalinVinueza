0d014f92f996e964b6482c05a26dffb3d7b5add9
StalinVinueza <csvinueza@puce.edu.ec>
2024-07-13 13:44:25 -0500

    sabado

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    tytle = models.CharField(max_length=30, null=False)
+    MOVIES_GENDER={
+        ('A','Animación'),
+        ('c','Comedia'),
+        ('F','Familiar'),
+        ('H','Heroes'),
+        ('S','Suspenso'),
+        ('T','Terror'),
+    }
+    
+    
+    gender = models.CharField(max_length=30, choices=MOVIES_GENDER, null=False)
+    director = models.CharField(max_length=30, null=False)
+    year = models.BigIntegerField(null=False)
+    synopsis = models.CharField(max_length=100, null=False)
+    
+    def __str__(self) -> str:
+        return self.tytle
+    
+    
+    
